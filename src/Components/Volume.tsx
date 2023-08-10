892832199201ca583efcca410d52438d0d8c03e2
harshal98 <53562369+harshal98@users.noreply.github.com>
2023-08-10 16:29:11 +0530

    Updated syntax error

--- src/Components/Volume.tsx
+++ src/Components/Volume.tsx
@@ -43,7 +43,7 @@ export default function Volume() {
     }
     useEffect(()=>{
          getData();
-            })
+          
     },[period]);
 
     useEffect(()=>console.log(vollist),[vollist]
